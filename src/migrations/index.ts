78826ce6b9bd7fd7f346ebe5b63a048314e2162d
Vikram <hello@vikramk.dev>
2026-01-17 20:07:05 -0500

    Move old migrations. Create new empty migration.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,33 +1,9 @@
-import * as migration_20251030_154608 from './20251030_154608';
-import * as migration_20251114_231544 from './20251114_231544';
-import * as migration_20251116_161250 from './20251116_161250';
-import * as migration_20260117_075704 from './20260117_075704';
-import * as migration_20260117_180933 from './20260117_180933';
+import * as migration_20260118_010053 from './20260118_010053';
 
 export const migrations = [
   {
-    up: migration_20251030_154608.up,
-    down: migration_20251030_154608.down,
-    name: '20251030_154608',
-  },
-  {
-    up: migration_20251114_231544.up,
-    down: migration_20251114_231544.down,
-    name: '20251114_231544',
-  },
-  {
-    up: migration_20251116_161250.up,
-    down: migration_20251116_161250.down,
-    name: '20251116_161250',
-  },
-  {
-    up: migration_20260117_075704.up,
-    down: migration_20260117_075704.down,
-    name: '20260117_075704',
-  },
-  {
-    up: migration_20260117_180933.up,
-    down: migration_20260117_180933.down,
-    name: '20260117_180933'
+    up: migration_20260118_010053.up,
+    down: migration_20260118_010053.down,
+    name: '20260118_010053'
   },
 ];
